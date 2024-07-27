eebd6969d6af6abd70512f2e54191d3526bfb49a
obynonwane <biostechnologyng@gmail.com>
2024-07-27 12:33:18 +0100

    calling chainlink pricing oracle

--- FundMe.sol
+++ FundMe.sol
@@ -2,18 +2,34 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18; // stating our solidity version
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
+uint256 public minimumUsd = 5e18;
+address [] funders;
+mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
     //make a function payable to allow it to accept native blockchain currency
     function fund() public payable {
-        // Allow users to sent $
-        // Have a minimum $ sent 
-
         // Validation to set minim required qwei
-        require(msg.value > 1e18, "did'nt send enough ETH"); // 1e18 = 1ETH = 1000000000000000000
+        require(getConversionrate(msg.value) > minimumUsd, "did'nt send enough ETH"); // 1e18 = 1ETH = 1000000000000000000
+
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
-    function withdraw() public{}
+    function getPrice() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        ( ,int price,,,) = priceFeed.latestRoundData();
+       return uint256(price * 1e10);
+    }
+
+    
+    function getConversionrate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e10;
+        return ethAmountInUsd;
+
+    }
 
 }
