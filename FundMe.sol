ebc765390698da3b8e694971a804b6221ca8fccd
obynonwane <biostechnologyng@gmail.com>
2024-07-27 14:53:04 +0100

    added function to with funds

--- FundMe.sol
+++ FundMe.sol
@@ -8,10 +8,15 @@ import {PriceConverter} from "./PriceConverter.sol";
 contract FundMe {
 
 using PriceConverter for uint256;
+
 uint256 public minimumUsd = 5e18;
+
 address [] funders;
+
 mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
     //make a function payable to allow it to accept native blockchain currency
+
+
     function fund() public payable {
         // Validation to set minim required qwei
         require(msg.value.getConversionrate() > minimumUsd, "did'nt send enough ETH"); // 1e18 = 1ETH = 1000000000000000000
@@ -20,5 +25,15 @@ mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
+
+    function withdraw() public{
+        for(uint256 fundIndex = 0; fundIndex < funders.length; fundIndex++ ){
+            address funder = funders[fundIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        // Reset the funders array to start at lenght zero
+        funders = new address[](0);
+    }
 
 }
