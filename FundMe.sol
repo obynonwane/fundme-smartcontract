d9aed72bcf8403d72705f319e11ba70931239a30
obynonwane <biostechnologyng@gmail.com>
2024-07-27 19:03:15 +0100

    added custom error

--- FundMe.sol
+++ FundMe.sol
@@ -5,26 +5,29 @@ pragma solidity ^0.8.18; // stating our solidity version
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
+// custom error handling
+error NotOwner();
+
 contract FundMe {
 
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     address [] funders;
 
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
     
     // state variable to hold address of the deployer
-    address public owner;
+    address public immutable i_owner;
     constructor(){
         //deployer of the contract
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
         // Validation to set minim required qwei
-        require(msg.value.getConversionrate() > minimumUsd, "did'nt send enough ETH"); // 1e18 = 1ETH = 1000000000000000000
+        require(msg.value.getConversionrate() > MINIMUM_USD, "did'nt send enough ETH"); // 1e18 = 1ETH = 1000000000000000000
 
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
@@ -50,7 +53,7 @@ contract FundMe {
     }
 
     modifier onlyOwner(){
-         require(msg.sender == owner, "sender is not owner");
+       if(msg.sender != i_owner){ revert NotOwner(); }
          _;
     }
 
